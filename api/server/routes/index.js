8d2f08f4c623b7cf12904208b88abd0e0ca4ea13
mikeias-silva <mikeias26@gmail.com>
2022-05-31 07:16:56 -0300

    refatorando local das pastas

--- api/server/routes/index.js
+++ api/server/routes/index.js
@@ -5,7 +5,6 @@ const route = new Router();
 route.get('/', (req, res) => res.send("oi"))
 
 route.get('/tempo_pg', (req, res) => {
-
     let token = `a5f2fb1bbbaa03ac4d06c69353d78eeb`
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=-25.094994&lon=-50.159667&appid=${token}`;
 
@@ -16,7 +15,6 @@ route.get('/tempo_pg', (req, res) => {
         .catch(function (error) {
             res.json(error)
         })
-
 });
 
 
