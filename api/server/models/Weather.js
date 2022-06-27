6eb9f7d1f12a975793f7db40dc7393f5472380af
mikeias-silva <mikeias26@gmail.com>
2022-06-26 23:10:41 -0300

    feature: model weather

---
+++ api/server/models/Weather.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+const env = dotenv.config().parsed;
+
+const Weather = {
+    async byCity(city) {
+        let token = `${env.API_TOKEN_OPENWEATHER}`;
+        let app_url = `${env.APP_URL}`;
+        let url = `${app_url}q=${city}&appid=${token}`;
+        let data = axios.get(url)
+        let response = await data;
+        return response;
+    }
+}
+
+export default Weather;
