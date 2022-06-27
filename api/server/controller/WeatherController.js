87dae0508a31b048de949257910b39c42105816d
mikeias-silva <mikeias26@gmail.com>
2022-06-26 23:10:34 -0300

    refatorado para model

--- api/server/controller/WeatherController.js
+++ api/server/controller/WeatherController.js
@@ -1,16 +1,11 @@
 
 import axios from 'axios';
-
+import Weather from '../models/Weather.js';
 const WeatherController = {
   byCity(request, response, next) {
     const city = request.params.city;
-    let token = `a5f2fb1bbbaa03ac4d06c69353d78eeb`
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${token}`;
-
-    axios.get(url)
-      .then(result =>
-        response.json(result.data)
-      )
+    Weather.byCity(city)
+      .then(result => response.json(result.data))
       .catch(next)
   }
 }
