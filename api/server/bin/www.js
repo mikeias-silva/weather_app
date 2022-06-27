5e54adedc359a2e8231ade2bfd055cd426eb21d1
mikeias-silva <mikeias26@gmail.com>
2022-06-26 23:10:06 -0300

    refactor: adicionado dotenv config

--- api/server/bin/www.js
+++ api/server/bin/www.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 import app from "../app.js";
+import dotenv from 'dotenv';
+const env = dotenv.config().parsed;
 
-const server = app.listen(3000, () => console.log('servidor iniciado'));
+const server = app.listen(env.PORT, () => console.log(`servidor iniciado na porta ${env.PORT}`));
 server.on('error', (err) => console.log(err))
