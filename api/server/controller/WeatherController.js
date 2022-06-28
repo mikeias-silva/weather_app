67eeb25c005559bf91c720ca5c21067fcfe77c4b
mikeias-silva <mikeias26@gmail.com>
2022-06-27 23:36:13 -0300

    refatorando

--- api/server/controller/WeatherController.js
+++ api/server/controller/WeatherController.js
@@ -7,19 +7,6 @@ const WeatherController = {
     Weather.byCity(city)
       .then(result => {
         response.json(result)
-        let options = { year: 'numeric', month: '2-digit', day: '2-digit', timeZone: 'UTC', hour12: false };
-        let nascerSol = new Date(result.sys.sunrise * 1000)
-        let porSol = new Date(result.sys.sunset * 1000);
-        let timezone = new Date(result.timezone * 1000);
-        console.clear()
-        console.log("*** Nascer do Sol **")
-        console.log(nascerSol.toLocaleDateString())
-        console.log(nascerSol.toLocaleTimeString())
-
-
-        console.log("*** Pôr do Sol **")
-        console.log(porSol.toLocaleDateString());
-        console.log(porSol.toLocaleTimeString())
       })
       .catch(next)
   }
