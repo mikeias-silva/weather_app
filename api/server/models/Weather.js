11685076e10b6633959c1cb5512b9024a82c1c9a
mikeias-silva <mikeias26@gmail.com>
2022-06-28 12:15:44 -0300

    previsao do tempo na onecall

--- api/server/models/Weather.js
+++ api/server/models/Weather.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
 const env = dotenv.config().parsed;
+const token = `${env.API_TOKEN_OPENWEATHER}`;
 
 const Weather = {
     async byCity(city) {
-        let token = `${env.API_TOKEN_OPENWEATHER}`;
         let app_url = `${env.APP_URL}`;
         let url = `${app_url}q=${city}&appid=${token}&units=metric&lang=pt_br`;
-        // let url = `https://api.openweathermap.org/data/2.5/onecall?lat=-25.094994&lon=-50.159667&appid=${token}&lang=pt_br&units=metric`
         let data = axios.get(url)
         let response = await data;
         const dados = Weather.tempoDia(response.data);
@@ -46,26 +45,78 @@ const Weather = {
         return tempo
     },
     async previsaoTempo(city) {
-        const token = `${env.API_TOKEN_OPENWEATHER}`;
+        const coordenadas = await Weather.getCoordenadas(city)
+        const urlPrevisao = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordenadas.lat}&lon=${coordenadas.lon}&appid=${token}&lang=pt_br&units=metric`
+        let dataPrevisao = axios.get(urlPrevisao)
+        let responsePrevisao = await dataPrevisao
+        let dados = Weather.parsePrevisaoTempo(responsePrevisao.data);
+        return responsePrevisao.data
+    },
+    async getCoordenadas(city) {
         const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${token}&limit=1`;
-
         let data = axios.get(url);
-        // console.log(url)
         let response = await data;
         let coordenadas = {
             "lat": response.data[0].lat,
             "lon": response.data[0].lon
         }
+        return coordenadas
+    },
+    parsePrevisaoTempo(data) {
 
-        console.log(coordenadas.lat)
-        const urlPrevisao = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordenadas.lat}&lon=${coordenadas.lon}&appid=${token}&lang=pt_br&units=metric`
-        let dataPrevisao = axios.get(urlPrevisao)
+        let minutos = data.minutely;
 
-        let responsePrevisao = await dataPrevisao
+        const newMinutos = minutos.map((minuto) => ({ "minuto": minutoAMinuto(minuto), "precipitacao": minuto.precipitation }))
 
+        function minutoAMinuto(minuto) {
+            let minutoParse = new Date(minuto.dt * 1000).toLocaleTimeString();
+            return minutoParse
+        }
 
-        return responsePrevisao.data
+        function horaEmHoraParse(hora) {
+            let horaParse = new Date(hora * 1000).toLocaleString();
+            return horaParse
+        }
+        let proximasHoras = data.hourly
+        const horaEmHora = proximasHoras.map(function (hora, index) {
+            index = {
+                "hora": horaEmHoraParse(hora.dt),
+                "temperatura": hora.temp,
+                "sensacao_termica": hora.feels_like,
+                "umidade": hora.humidity,
+                "nuvens": hora.clouds,
+                "tempo": hora.weather[0].description,
+                "icon": hora.weather[0].icon,
+                "probabilidade": hora.pop,
 
+            }
+            if (hora.hasOwnProperty('rain')) {
+                index.rain = hora.rain
+            }
+            return index
+        });
+
+        // console.log(horaEmHora)
+        const previsaoTempo = {
+            "atual": {
+                "dataHoraAtualizacao": new Date(data.current.dt * 1000).toLocaleString(),
+                "nascerSol": new Date(data.current.sunrise * 1000).toLocaleTimeString(),
+                "porSol": new Date(data.current.sunset * 1000).toLocaleTimeString(),
+                "temperatura": data.current.temp,
+                "sensacaoTermica": data.current.feels_like,
+                "pressao": data.current.pressure,
+                "umidade": data.current.humidity,
+                "nuvens": data.current.clouds,
+                "velocidadeVento": data.current.wind_speed,
+                "tempo": data.current.weather[0].description,
+                "icon": data.current.weather[0].icon,
+            },
+            "minutoAMinuto": newMinutos,
+            "proximas48Horas": horaEmHora,
+            
+        }
+
+        console.log(previsaoTempo);
     }
 }
 
