dc8bcdddd35afea5bb1a566aef0f2503a0957b14
mikeias-silva <mikeias26@gmail.com>
2022-06-28 22:59:03 -0300

    novos componentes

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Search from './Search.js'
+import TempoDia from './TempoDia.js'
 
 const Home = () => {
   return (
-    
+
     <div>
-        <h1>Home</h1>
+      <h1>Home</h1>
+      <Search />
+      <p>Nome Cidade</p>
+      <TempoDia />
     </div>
 
   )
---
+++ client/src/components/TempoDia.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const TempoDia = () => {
+  return (
+    <div>
+        <ul>
+            <li>Temperatura</li>
+            <li>Maxima</li>
+            <li>Minima</li>
+            <li>Sensação Térmica</li>
+            <li>Descrição Tempo</li>
+            <li>Ventos</li>
+            <li>Chuva</li>
+            <li>Nascer Sol</li>
+            <li>Por Sol</li>
+
+        </ul>
+    </div>
+  )
+}
+
+export default TempoDia
